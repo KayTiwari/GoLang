24603195bf9fababc82d2a73d18bb07c878d337c
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-17 23:32:11 -0600

    var keyword

--- src/tutorial/varkeyword/varkeyword.go
+++ src/tutorial/varkeyword/varkeyword.go
@@ -2,9 +2,12 @@ package main
 
 import "fmt"
 
+//var keyword allows you to declare a variable outside a function body
 var y = 41
 
-//var keyword allows you to declare a variable outside a function body
+//declares a variable with the identifier z of the type int
+// default value of type int is 0
+var z int
 
 func main() {
 	//short declaration operator - x, 42 is an operand, := is the operator
