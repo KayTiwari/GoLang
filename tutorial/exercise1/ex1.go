94b0da53d1d82074fdcfb14c4a2dd564d763ec13
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-18 00:30:04 -0600

    pt ex2

---
+++ tutorial/exercise1/ex1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//short decl variables ex
+
+func main() {
+	a := 42
+	b := "James Bond"
+	c := true
+
+	fmt.Println(a, b, c)
+}
---
+++ tutorial/exercise1/ex2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//var keyword ex
+
+var a = int
+var b = string
+var c = bool
+
+func main() {
+	fmt.Println(a, b, c)
+}
