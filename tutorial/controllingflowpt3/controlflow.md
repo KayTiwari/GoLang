045fe646066d8c868b34d1ce2f2a3fc0618e6d74
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-25 22:28:42 -0600

    forloops and nested for loops

---
+++ tutorial/controllingflowpt3/controlflow.md
@@ -0,0 +1,6 @@
+control flow naturally flows top to bottom
+
+sequential: one action followed by another
+loop: same action repeated 
+condition: based on a condition do 'this' action
+
---
+++ tutorial/controllingflowpt3/loops/loops.go
@@ -0,0 +1,21 @@
+package main
+
+//gobyexample
+
+import "fmt"
+
+func main() {
+	// for
+	i := 1
+	for i <= 3 {
+		i++
+		fmt.Println(i)
+
+		// No 'while' in go
+	}
+
+	// for init; condition; post{
+	// }
+
+
+}
